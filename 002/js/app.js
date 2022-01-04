f574169c50d98ce195951c8382e26d4caae70e9c
dusuna <dusunax@gmail.com>
2022-01-05 01:01:07 +0900

    002 end

--- 002/js/app.js
+++ 002/js/app.js
@@ -1,11 +1,22 @@
 const canvas=document.getElementById('jsCanvas');
 const cContext=canvas.getContext('2d');
-canvas.width=700;
-canvas.height=700;
+const colors=document.querySelectorAll('.controls_color');
+const INITIAL_COLOR="#2c2c2c";
+const CANVAS_SIZE=700;
+const range=document.getElementById('jsRange');
+const mode=document.getElementById('jsMode');
+const btnSave=document.getElementById('jsSave');
+
+canvas.width=CANVAS_SIZE;
+canvas.height=CANVAS_SIZE;
+cContext.fillStyle="#fff";
+cContext.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
 cContext.lineWidth=2.5;
-cContext.strokeStyle="#2c2c2c";
+cContext.strokeStyle=INITIAL_COLOR;
+cContext.fillStyle=INITIAL_COLOR;
 
 let painting=false;
+let filling=false;
 function stopPainting(){
     painting=false;
 }
@@ -18,7 +29,6 @@ function onMouseDown(){
 function onMouseUp(){
     stopPainting();
 }
-
 function onMouseMove(event){
     const x=event.offsetX;
     const y=event.offsetY;
@@ -30,10 +40,55 @@ function onMouseMove(event){
         cContext.stroke();     //line에 stroke
     }
 }
-
+function handleColorChange(event){
+    const color=event.target.style.backgroundColor;
+    cContext.strokeStyle=color;
+    cContext.fillStyle=cContext.strokeStyle;
+}
+function handleRangeChange(event){
+    const stroke_size=event.target.value;
+    cContext.lineWidth=stroke_size;
+}
+function handleModeChange(event){
+    if(filling){
+        filling=false;
+        mode.innerText="그리기";
+        
+    } else {
+        filling=true;
+        mode.innerText="채우기";
+    }
+}
+function handleCanvasClick(){
+    if(filling){
+        cContext.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
+    }
+}
+function handleCM(event){
+    event.preventDefault();
+}
+function handleSaveClick(){
+    const img=canvas.toDataURL();
+    const link=document.createElement("a");
+    link.href=img;
+    link.download="PainJS";
+    link.click();
+}
+Array.from(colors).forEach(color_pick => color_pick.addEventListener("click", handleColorChange));
 if(canvas){
     canvas.addEventListener("mousemove", onMouseMove);
     canvas.addEventListener("mousedown", startPainting);
     canvas.addEventListener("mouseup", stopPainting);
     canvas.addEventListener("mouseleave", stopPainting);
+    canvas.addEventListener("click", handleCanvasClick);
+    canvas.addEventListener("contextmenu", handleCM);
+}
+if(range){
+    range.addEventListener("input", handleRangeChange);
+}
+if(mode){
+    mode.addEventListener("click", handleModeChange);
+}
+if(btnSave){
+    btnSave.addEventListener("click", handleSaveClick);
 }
