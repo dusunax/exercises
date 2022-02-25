fada4a94eabb38b73c1ff491b0d0bba72ef3906f
Dusuna <94776135+dusunax@users.noreply.github.com>
2022-02-25 10:20:28 +0900

    input readonly 등, 키보드 살까

--- 008/index.js
+++ 008/index.js
@@ -4,14 +4,24 @@ let chk_compelete=false;
 // 이벤트핸들
 function handle_keydown(e) {
   // 확인용 console.log(e.keyCode)
+  console.log(`key: ${e.key}, keycode: ${e.keyCode}`);
   event.preventDefault;
   key_pressAnimation();
   printHere.value = e.key;
   chk_compelete=false;
-  chk_compelete=chk_by_switch(e.keyCode);
-  if(!chk_compelete){
-    chk_by_forEach(e.key)
+  if(e.keyCode == 67){
+    document.querySelector('.c').classList.add('active');
+    printHere.value = "c";
+    return;
   }
+  if(!chk_compelete){
+    chk_by_forEach(e.key);
+  }
+  if(!chk_compelete){
+    chk_compelete=chk_by_switch(e.keyCode);
+    chk_compelete=true;
+  }
+  key_Unactive();
 }
 // 함수시작
 function chk_by_switch(code){
@@ -19,53 +29,43 @@ function chk_by_switch(code){
   switch (code) {
     case 32:
       document.querySelector('.SpaceBar').classList.add('active');
-      key_Unactive();
       printHere.value = "SpaceBar";
-      return chk_compelete=true;
+      break;
     case 9:
       document.querySelector('.Tab').classList.add('active');
-      key_Unactive();
-      return chk_compelete=true;
+      break;
     case 16:
       document.querySelector('.LeftShift').classList.add('active');
-      key_Unactive();
-      return chk_compelete=true;
+      break;
     case 18:
       document.querySelector('.LeftAlt').classList.add('active');
-      key_Unactive();
-      return chk_compelete=true;
+      break;
     case 17:
       document.querySelector('.LeftControl').classList.add('active');
-      key_Unactive();
-      return chk_compelete=true;
+      break;
     case 20:
       document.querySelector('.CapsLock').classList.add('active');
-      key_Unactive();
-      return chk_compelete=true;
+      break;
     case 91:
       document.querySelector('.cmd').classList.add('active');
-      key_Unactive();
       printHere.value = "CMD";
-      return chk_compelete=true;
+      break;
     case 144:
       document.querySelector('.NumLk').classList.add('active');
-      key_Unactive();
-      return chk_compelete=true;
     default:
   };
 }
 function chk_by_forEach(ekey){
-  console.log('forEach', ekey);
+  // console.log('forEach', ekey);
   keys.forEach((key) => {
-    key_Unactive();
     const lower_eKey = ekey.toLowerCase();
     const lower_keyText = key.innerText.toLowerCase();
     if (lower_eKey == lower_keyText) {
       key.classList.add('active');
-      return;
+      return chk_compelete=true;
     } else if (key.classList.contains(lower_eKey)) {
       key.classList.add('active');
-      return;
+      return chk_compelete=true;
     }
   });
 }
