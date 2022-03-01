e31bf5464234c28be1f4e931537bbaa0a526c194
Dusuna <94776135+dusunax@users.noreply.github.com>
2022-03-01 23:25:09 +0900

    010 껍데기 1인분

---
+++ 010/assets/js/data.js
@@ -0,0 +1,44 @@
+let data=[
+    [
+        {
+            test_name: '정보처리기사',
+            test_type: '필기',
+            test_date: '3월 5일',
+            test_place: '서울국가자격시험장(휘경동)',
+            score: [
+                {
+                    score_date: '2022/03/01',
+                    score_desc: '2021년 1회',
+                    score_scroe: '70'
+                },
+                {
+                    score_date: '2022/03/01',
+                    score_desc: '2021년 2회',
+                    score_scroe: '50'
+                }
+            ],
+            show_first: true
+        },
+    ],
+    [
+        {
+            test_name: '정보처리기능사',
+            test_type: '실기',
+            test_date: '3월 20일',
+            test_place: '서울국가자격시험장(휘경동)',
+            score: [
+                {
+                    score_date: '2022/03/02',
+                    score_desc: '2021년 1회',
+                    score_scroe: '70'
+                },
+                {
+                    score_date: '2022/03/02',
+                    score_desc: '2021년 2회',
+                    score_scroe: '50'
+                }
+            ],
+            show_first: false
+        },
+    ]
+]
