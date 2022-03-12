12dc8d4cd31790fbc685ec35728d280a51db1726
Dusuna <94776135+dusunax@users.noreply.github.com>
2022-03-13 04:20:07 +0900

    011 취침

--- 011/qna.js
+++ 011/qna.js
@@ -4,9 +4,14 @@ const btn_retry = document.querySelector('.btn_retry');
 const yesNo_arr=['맞아', '아니야', '글쎄(다른 질문!)']
 let level=0, this_idx, currIdx=0;
 let answers=0;
+let level_limit=2;
+const result_title = document.querySelector('.result_title');
+const result_img = document.querySelector('.img_box');
+const result_desc = document.querySelector('.result_desc');
 // init
 const qna_box = document.querySelector('.qna_box');
 const qna_answer=document.querySelectorAll('.qna_answer');
+const qna_lenth=qna_answer.length;
 const result = document.querySelector('.result');
 qna_answer.forEach((e)=>{
     e.addEventListener('click', qnaClick_handle);
@@ -22,38 +27,33 @@ function qnaInit(){
 function qnaClick_handle(){
     this_idx = getIndex(getSibling(this), this);
     let yesNo = (this_idx==0)?true:false;
-    if(level == 0){
+    let next = (this_idx==2)?true:false;
+    // console.log(this_idx, level, yesNo, next);
+    if( level == 0 ){
         qna_nextLevel();
         qna_going(this_idx);
-        currIdx = this_idx;
-        answers += this_idx;
-    } else if (level<2){
-        if(yesNo){
-            qna_nextLevel();    // level += 1
-            qna_going(answers);        // 출력
-            answers += currIdx;
-        } else {                // 아니오 클릭
-            if(currIdx==3){
-                currIdx=-1;
-            }
-            qna_going(currIdx+=1);
-        }
-    } else {
+        return;
+    } else if( level > level_limit-1 && !next){
         if(yesNo){
             answers += 1;
         }
         qna_ended();
-    };
+        return;
+    }
+    if(yesNo){
+        qna_nextLevel();
+        qna_going(answers);
+    } else if(next){
+        qna_goingNext();
+    } else {
+        qna_goingNext();
+    }
     console.log('클릭:', this_idx, "\nlevel:", level, '\n지금:', currIdx, '\n합: ', answers);
 };
 // QNA process
-function qna_going(idx){
-    let target=qna[level][idx];
-    qna_print(target);
-};
 function qna_print(target){
     let count=0;
-    stage_title.innerHTML=`<strong>Yes: ${level-1}/2.</strong><br> ${target.text}`
+    stage_title.innerHTML=`<strong>level: ${level-1}/2.</strong><br> ${target.text}`
     qna_answer.forEach(li => {
         li.innerHTML=`${yesNo_arr[count++]}`
         if(count>3){
@@ -61,16 +61,36 @@ function qna_print(target){
         }
     });
 };
+function qna_going(idx){
+    answers += idx;
+    let target=qna[level][idx];
+    qna_print(target);
+};
+function qna_goingNext(){
+    if(currIdx==3){
+        currIdx=-1;
+    }
+    let target=qna[level][currIdx+=1];
+    qna_print(target);
+};
 function qna_nextLevel(){
     level+=1;
 };
+// 결과
 function qna_ended(){
     stage_title.innerHTML=`내가 좋아하는 캡슐커피 스타일은?!`
     qna_box.classList.remove('on');
     result.classList.add('on');
     console.log(answers+"\n결과: "+(answers+1)+'/8');
-    document.querySelector('.img_box').innerHTML="결과 "+(answers)+"번째"
+    document.querySelector('.img_box').innerHTML="결과 "+(answers)+"번째";
+    result_print(answers);
 };
+function result_print(idx){
+    console.log(answer[idx]);
+    result_title.innerHTML=`${answer[idx].title}`
+    result_desc.innerHTML=`${answer[idx].desc}`
+    result_img.innerHTML=`${idx}번째 이미지`
+}
 // redirect
 btn_retry.addEventListener('click', function(){
     location.reload();
