2e7d68386388e12c33921356665b4df0d939abc6
Dusuna <94776135+dusunax@users.noreply.github.com>
2022-06-29 23:59:46 +0900

    018 중간저장

--- 018/assets/js/weather.js
+++ 018/assets/js/weather.js
@@ -1,10 +1,10 @@
 // temp key: dusuna
 const API_KEY='a2e8922539a2e4f37866cc7d173c8be2'
 // 
-
+const weatherResetTime=30*1000
 setInterval(()=>{
     askForCoords();
-}, 1000)
+}, weatherResetTime)
 askForCoords();
 function askForCoords() {
     navigator.geolocation.getCurrentPosition(handleGeoSucces, handleGeoError);
@@ -19,7 +19,7 @@ function handleGeoError() {
 }
 
 function getWeather(lat, lon) {
-    console.log("현재 좌표: " + lat, lon);
+    // console.log("현재 좌표: " + lat, lon);
     fetch(
             `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
         )
@@ -36,9 +36,12 @@ function getWeather(lat, lon) {
 
             weather.innerHTML =
                 `
-            <strong>${temparature}℃</strong>
-            <img src="https://ssl.gstatic.com/onebox/weather/64/${string}.png" />`;
-
+            <strong>${temparature.toFixed(1)}℃</strong>
+            <img src="http://openweathermap.org/img/wn/${icon}@2x.png" />`;
+            
+            
+            // <img src="https://ssl.gstatic.com/onebox/weather/64/${string}.png" />
+            // <img src="http://openweathermap.org/img/wn/${icon}.png"
             // <img src="http://openweathermap.org/img/w/${icon}.png"
             // <span>도시: ${place}</span>
         });
