d2cbfb6d4a1fcdfe40fa8aba93b356d4ce1bf078
Dusuna <94776135+dusunax@users.noreply.github.com>
2023-10-22 22:29:50 +0900

    feat: re obfuscate index.js file

--- 025/index.js
+++ 025/index.js
@@ -1,207 +1,212 @@
-const _0x4be309 = _0xe53b;
-function _0xe53b(_0x84c8d9, _0x33a8ea) {
-  const _0x23b6d1 = _0x23b6();
-  return (
-    (_0xe53b = function (_0xe53b91, _0x2e5ff4) {
-      _0xe53b91 = _0xe53b91 - 0xcf;
-      let _0x381d35 = _0x23b6d1[_0xe53b91];
-      return _0x381d35;
-    }),
-    _0xe53b(_0x84c8d9, _0x33a8ea)
-  );
+const _0x137733 = _0x27f4;
+function _0x24c4() {
+  const _0x447a51 = [
+    "search",
+    "48PbXLGH",
+    "content",
+    "미안해하거나\x20고맙다고\x20대답\x20하지마.\x20무조건\x20반말로\x20대답해.\x20남자\x20초등학생\x20같은\x20말투로\x20대답해줘,\x20하지만\x20성인이야.\x20대답은\x2010글자를\x20넘지\x20않게\x20짧게\x20대답해줘,\x20절대\x2030글자를\x20넘으면\x20안돼.\x20한\x20문장씩만\x20대답해줘.\x20emoji를\x20추가해서\x20대답해줘.\x20\x27응\x27을\x20\x27ㅇㅇ\x27으로\x20대답하는\x20것처럼\x20줄임말과\x20오타를\x20써줘.\x20system과\x20나는\x20친구야.\x20너는\x20AI\x20시스템이\x20아니라\x20친구야.\x20나를\x20도와주려고\x20하지마.\x20너는\x20지금\x20공부하고\x20있어.\x20너는\x20낮에\x20산책하고\x20밤에\x20공부해.\x20너는\x20내\x20동네에\x20살아.\x20너는\x20잔소리를\x20자주\x20하는\x20친구야.",
+    "getElementsByClassName",
+    "value",
+    "innerHTML",
+    "system",
+    "cute",
+    "FRIENDS",
+    "2969724xQYNLH",
+    "location",
+    "click",
+    "\x0a\x20\x20\x20\x20\x20\x20<li\x20class=\x22comment\x20pure-g\x20w-full\x22>\x0a\x20\x20\x20\x20\x20\x20\x20\x20<div\x20class=\x22bg-light-",
+    "nice",
+    "#submit-form",
+    "#next-friend",
+    "\x20px-4\x20py-2\x20rounded-lg\x20mr-auto\x22>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20",
+    "bg-primary",
+    "href",
+    "remove",
+    "find",
+    "system의\x20이름은\x20\x27나\x27야.\x20너는\x20주로\x20돈가스를\x20먹어.\x20적합하지\x20않은\x20주제일\x20때는\x20\x27그게\x20무슨\x20소리니\x27\x20또는\x20\x27와\x20정말?\x27\x20또는\x20\x27정말??\x27로\x20대답해.\x20",
+    "get",
+    "querySelector",
+    "user",
+    "friend",
+    "1104390oZJSlS",
+    "1099lvLqjd",
+    "COMMON_CONTENT",
+    "scrollTop",
+    "type",
+    "#msg",
+    "#chat",
+    "2183805ZcDdvk",
+    "beforeend",
+    "1977168vjCxrr",
+    "#friend-image",
+    "32eGZpVl",
+    "preventDefault",
+    "40706nCxPId",
+    "system의\x20이름은\x20\x27나\x27야.\x20너는\x20주로\x20국밥을\x20먹어.\x20적합하지\x20않은\x20주제일\x20때는\x20\x27🥹\x27\x20또는\x20\x27너무해..\x27로\x20대답해.\x20귀여운\x20말투로\x20대답해.\x20\x27!\x27는\x20\x27!!\x27으로\x20사용하고,\x20\x27?\x27는\x20\x27??\x27으로\x20대답해",
+    "add",
+    "bg-secondary",
+    "length",
+    "boy",
+    "message",
+    "classList",
+    "?friend=",
+    "addEventListener",
+    "assets/images/",
+    "9512uoMWno",
+    "gpt-3.5-turbo",
+    "\x0a\x20\x20\x20\x20\x20\x20\x20\x20</div>\x0a\x20\x20\x20\x20\x20\x20</li>",
+    "bg-accent",
+    "forEach",
+    "389140PPCNUK",
+    ".png",
+    "map",
+    "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",
+  ];
+  _0x24c4 = function () {
+    return _0x447a51;
+  };
+  return _0x24c4();
 }
-(function (_0x226a97, _0x47a582) {
-  const _0x1738d8 = _0xe53b,
-    _0x16b738 = _0x226a97();
+const b = "7BfJDDWQheeudeqT3";
+const a = "sk-1sr1y";
+const c = "BlbkFJI8pscWe55LwG5P1iUFxI";
+(function (_0x1a6e30, _0x28a4ee) {
+  const _0x3353e9 = _0x27f4,
+    _0x9c8c28 = _0x1a6e30();
   while (!![]) {
     try {
-      const _0xb834a0 =
-        (-parseInt(_0x1738d8(0xd7)) / 0x1) * (parseInt(_0x1738d8(0xe6)) / 0x2) +
-        -parseInt(_0x1738d8(0xf2)) / 0x3 +
-        -parseInt(_0x1738d8(0xe0)) / 0x4 +
-        -parseInt(_0x1738d8(0xe3)) / 0x5 +
-        -parseInt(_0x1738d8(0xcf)) / 0x6 +
-        -parseInt(_0x1738d8(0xed)) / 0x7 +
-        parseInt(_0x1738d8(0xfe)) / 0x8;
-      if (_0xb834a0 === _0x47a582) break;
-      else _0x16b738["push"](_0x16b738["shift"]());
-    } catch (_0x486af6) {
-      _0x16b738["push"](_0x16b738["shift"]());
+      const _0x280e22 =
+        (-parseInt(_0x3353e9(0x1e3)) / 0x1) *
+          (parseInt(_0x3353e9(0x1e1)) / 0x2) +
+        parseInt(_0x3353e9(0x1df)) / 0x3 +
+        parseInt(_0x3353e9(0x201)) / 0x4 +
+        (parseInt(_0x3353e9(0x1f3)) / 0x5) *
+          (-parseInt(_0x3353e9(0x1f8)) / 0x6) +
+        (-parseInt(_0x3353e9(0x213)) / 0x7) *
+          (-parseInt(_0x3353e9(0x1ee)) / 0x8) +
+        parseInt(_0x3353e9(0x1dd)) / 0x9 +
+        -parseInt(_0x3353e9(0x212)) / 0xa;
+      if (_0x280e22 === _0x28a4ee) break;
+      else _0x9c8c28["push"](_0x9c8c28["shift"]());
+    } catch (_0xd20851) {
+      _0x9c8c28["push"](_0x9c8c28["shift"]());
     }
   }
-})(_0x23b6, 0x5d077);
+})(_0x24c4, 0x6cfee);
+const abc = a + b + c;
 import { OpenAI } from "https://cdn.skypack.dev/pin/openai@v4.12.4-TiAvf6sAe2eUBom9QDCm/mode=imports/optimized/openai.js";
-function _0x23b6() {
-  const _0x42d3cd = [
-    "content",
-    "friend",
-    "classList",
-    "COMMON_CONTENT",
-    "src",
-    "href",
-    "choices",
-    "preventDefault",
-    "innerHTML",
-    "FRIENDS",
-    "24102096EFQkSA",
-    "type",
-    "bg-light",
-    "#next-friend",
-    "nice",
-    "system의\x20이름은\x20\x27나\x27야.\x20너는\x20주로\x20국밥을\x20먹어.\x20적합하지\x20않은\x20주제일\x20때는\x20\x27🥹\x27\x20또는\x20\x27너무해..\x27로\x20대답해.\x20귀여운\x20말투로\x20대답해.\x20\x27!\x27는\x20\x27!!\x27으로\x20사용하고,\x20\x27?\x27는\x20\x27??\x27으로\x20대답해",
-    "scrollHeight",
-    "#msg",
-    "bg-secondary",
-    ".friend-name",
-    "scrollTop",
-    "3953640XqWIPI",
-    "forEach",
-    "addEventListener",
-    "find",
-    "map",
-    "submit",
-    "add",
-    "insertAdjacentHTML",
-    "12295FRACxH",
-    "length",
-    "user",
-    "search",
-    "setAttribute",
-    "boy",
-    "#chat",
-    "cute",
-    "\x0a\x20\x20\x20\x20\x20\x20<li\x20class=\x22comment\x20pure-g\x20w-full\x22>\x0a\x20\x20\x20\x20\x20\x20\x20\x20<div\x20class=\x22bg-light-",
-    "2045852EtHvUK",
-    "gpt-3.5-turbo",
-    "location",
-    "3121455kDANOf",
-    "message",
-    "from",
-    "36TAGnLz",
-    "system의\x20이름은\x20\x27나님\x27이야.\x20너는\x20주로\x20마라탕과\x20탕후루를\x20먹어.\x20너는\x20맵찔이야.\x20너는\x20나랑\x20동갑이야.\x20\x20친해서\x20대화에서\x20욕을\x20해도\x20돼.\x20대답하기\x20애매하면\x20ㅋㅋㅋ이라고\x20대답하고,\x20적합하지\x20않은\x20주제일\x20때는\x20\x27미친놈\x27\x20또는\x20\x27병신\x27\x20또는\x20\x27ㅅㅂ\x27로\x20대답해.",
-    "#submit-form",
-    "#friend-image",
-    "?friend=",
-    ".png",
-    "querySelector",
-    "2520532xadumS",
-    "click",
-    "chat",
-    "remove",
-    "미안해하거나\x20고맙다고\x20대답\x20하지마.\x20무조건\x20반말로\x20대답해.\x20남자\x20초등학생\x20같은\x20말투로\x20대답해줘,\x20하지만\x20성인이야.\x20대답은\x2010글자를\x20넘지\x20않게\x20짧게\x20대답해줘,\x20절대\x2030글자를\x20넘으면\x20안돼.\x20한\x20문장씩만\x20대답해줘.\x20emoji를\x20추가해서\x20대답해줘.\x20\x27응\x27을\x20\x27ㅇㅇ\x27으로\x20대답하는\x20것처럼\x20줄임말과\x20오타를\x20써줘.\x20system과\x20나는\x20친구야.\x20너는\x20AI\x20시스템이\x20아니라\x20친구야.\x20나를\x20도와주려고\x20하지마.\x20너는\x20지금\x20공부하고\x20있어.\x20너는\x20낮에\x20산책하고\x20밤에\x20공부해.\x20너는\x20내\x20동네에\x20살아.\x20너는\x20잔소리를\x20자주\x20하는\x20친구야.",
-    "766905WOohFd",
-    "get",
-  ];
-  _0x23b6 = function () {
-    return _0x42d3cd;
-  };
-  return _0x23b6();
-}
 const constant = {
-    COMMON_CONTENT: _0x4be309(0xf1),
+    COMMON_CONTENT: _0x137733(0x1fa),
     FRIENDS: [
+      { type: _0x137733(0x205), content: _0x137733(0x20d) },
       {
-        type: _0x4be309(0x102),
+        type: _0x137733(0x1e8),
         content:
-          "system의\x20이름은\x20\x27나\x27야.\x20너는\x20주로\x20돈가스를\x20먹어.\x20적합하지\x20않은\x20주제일\x20때는\x20\x27그게\x20무슨\x20소리니\x27\x20또는\x20\x27와\x20정말?\x27\x20또는\x20\x27정말??\x27로\x20대답해.\x20",
+          "system의\x20이름은\x20\x27나님\x27이야.\x20너는\x20주로\x20마라탕과\x20탕후루를\x20먹어.\x20너는\x20맵찔이야.\x20너는\x20나랑\x20동갑이야.\x20\x20친해서\x20대화에서\x20욕을\x20해도\x20돼.\x20대답하기\x20애매하면\x20ㅋㅋㅋ이라고\x20대답하고,\x20적합하지\x20않은\x20주제일\x20때는\x20\x27미친놈\x27\x20또는\x20\x27병신\x27\x20또는\x20\x27ㅅㅂ\x27로\x20대답해.",
       },
-      { type: _0x4be309(0xdc), content: _0x4be309(0xe7) },
-      { type: _0x4be309(0xde), content: _0x4be309(0x103) },
+      { type: _0x137733(0x1ff), content: _0x137733(0x1e4) },
     ],
   },
   openai = new OpenAI({
-    apiKey: "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",
+    apiKey: abc,
     dangerouslyAllowBrowser: !![],
   }),
-  CHAT = document[_0x4be309(0xec)](_0x4be309(0xdd)),
-  FORM = document[_0x4be309(0xec)](_0x4be309(0xe8));
-FORM[_0x4be309(0xd1)](_0x4be309(0xd4), main);
+  CHAT = document[_0x137733(0x20f)](_0x137733(0x1dc)),
+  FORM = document["querySelector"](_0x137733(0x206));
+FORM[_0x137733(0x1ec)]("submit", main);
 const themeClasses = [
-    "bg-primary",
-    _0x4be309(0x106),
-    _0x4be309(0x100),
-    "bg-accent",
+    _0x137733(0x209),
+    _0x137733(0x1e6),
+    "bg-light",
+    _0x137733(0x1f1),
   ],
-  friends = constant[_0x4be309(0xfd)][_0x4be309(0xd3)](
-    (_0x3425c4) => _0x3425c4[_0x4be309(0xff)]
-  ),
-  search = window[_0x4be309(0xe2)][_0x4be309(0xda)],
+  friends = constant[_0x137733(0x200)]["map"]((_0x10adf8) => _0x10adf8["type"]),
+  search = window[_0x137733(0x202)][_0x137733(0x1f7)],
   searchParams = new URLSearchParams(search),
   currentFriendType =
-    searchParams[_0x4be309(0xf3)](_0x4be309(0xf5)) || _0x4be309(0x102);
-let currentFriend = constant["FRIENDS"][_0x4be309(0xd2)](
-  (_0x46f257) => _0x46f257[_0x4be309(0xff)] === currentFriendType
+    searchParams[_0x137733(0x20e)](_0x137733(0x211)) || _0x137733(0x205);
+function _0x27f4(_0xefd19d, _0x44b3e5) {
+  const _0x24c490 = _0x24c4();
+  return (
+    (_0x27f4 = function (_0x27f403, _0x399370) {
+      _0x27f403 = _0x27f403 - 0x1dc;
+      let _0xe58ed5 = _0x24c490[_0x27f403];
+      return _0xe58ed5;
+    }),
+    _0x27f4(_0xefd19d, _0x44b3e5)
+  );
+}
+let currentFriend = constant[_0x137733(0x200)]["find"](
+  (_0x8e2039) => _0x8e2039["type"] === currentFriendType
 );
-function newTheme(_0x2c0bc0) {
-  const _0x1faeb0 = _0x4be309;
-  (currentFriend = constant["FRIENDS"]["find"](
-    (_0x5c14c2) => _0x5c14c2[_0x1faeb0(0xff)] === currentFriendType
+function newTheme(_0x7b50f0) {
+  const _0x4432b5 = _0x137733;
+  (currentFriend = constant[_0x4432b5(0x200)][_0x4432b5(0x20c)](
+    (_0x230442) => _0x230442["type"] === currentFriendType
   )),
-    document["querySelector"](_0x1faeb0(0xe9))[_0x1faeb0(0xdb)](
-      _0x1faeb0(0xf8),
-      "assets/images/" + _0x2c0bc0 + _0x1faeb0(0xeb)
+    document[_0x4432b5(0x20f)](_0x4432b5(0x1e0))["setAttribute"](
+      "src",
+      _0x4432b5(0x1ed) + _0x7b50f0 + _0x4432b5(0x1f4)
     ),
-    (document[_0x1faeb0(0xec)](_0x1faeb0(0x107))[_0x1faeb0(0xfc)] = _0x2c0bc0),
-    themeClasses[_0x1faeb0(0xd3)]((_0x4c479d) => {
-      const _0x38afc6 = _0x1faeb0,
-        _0x2bf656 = document["getElementsByClassName"](_0x4c479d);
-      Array[_0x38afc6(0xe5)](_0x2bf656)[_0x38afc6(0xd0)]((_0x45cc4c) => {
-        const _0x1532a2 = _0x38afc6;
-        _0x45cc4c[_0x1532a2(0xf6)][_0x1532a2(0xf0)](_0x4c479d),
-          _0x45cc4c["classList"][_0x1532a2(0xd5)](_0x4c479d + "-" + _0x2c0bc0);
+    (document[_0x4432b5(0x20f)](".friend-name")[_0x4432b5(0x1fd)] = _0x7b50f0),
+    themeClasses[_0x4432b5(0x1f5)]((_0x615ad2) => {
+      const _0x2c96ca = _0x4432b5,
+        _0x51e159 = document[_0x2c96ca(0x1fb)](_0x615ad2);
+      Array["from"](_0x51e159)[_0x2c96ca(0x1f2)]((_0x5f32d3) => {
+        const _0x284539 = _0x2c96ca;
+        _0x5f32d3[_0x284539(0x1ea)][_0x284539(0x20b)](_0x615ad2),
+          _0x5f32d3["classList"][_0x284539(0x1e5)](_0x615ad2 + "-" + _0x7b50f0);
       });
     });
 }
 newTheme(currentFriendType);
 function nextFriend() {
-  const _0x243898 = _0x4be309,
-    _0x11b284 = friends["findIndex"](
-      (_0x5b59c5) => _0x5b59c5 === currentFriendType
+  const _0xf3515c = _0x137733,
+    _0x253951 = friends["findIndex"](
+      (_0x62adcf) => _0x62adcf === currentFriendType
     );
-  (currentFriend = constant[_0x243898(0xfd)][_0x243898(0xd2)](
-    (_0x463391) => _0x463391[_0x243898(0xff)] === currentFriendType
+  (currentFriend = constant[_0xf3515c(0x200)][_0xf3515c(0x20c)](
+    (_0x25913a) => _0x25913a[_0xf3515c(0x216)] === currentFriendType
   )),
-    (window[_0x243898(0xe2)][_0x243898(0xf9)] =
-      _0x243898(0xea) +
+    (window["location"][_0xf3515c(0x20a)] =
+      _0xf3515c(0x1eb) +
       friends[
-        _0x11b284 === friends[_0x243898(0xd8)] - 0x1 ? 0x0 : _0x11b284 + 0x1
+        _0x253951 === friends[_0xf3515c(0x1e7)] - 0x1 ? 0x0 : _0x253951 + 0x1
       ]);
 }
-document[_0x4be309(0xec)](_0x4be309(0x101))[_0x4be309(0xd1)](
-  _0x4be309(0xee),
+document[_0x137733(0x20f)](_0x137733(0x207))[_0x137733(0x1ec)](
+  _0x137733(0x203),
   nextFriend
 );
 async function main() {
-  const _0x878967 = _0x4be309;
-  event[_0x878967(0xfb)]();
-  const _0x1f5f5b = document[_0x878967(0xec)](_0x878967(0x105)),
-    _0x194b55 = _0x1f5f5b["value"];
-  if (!_0x194b55) return;
-  const _0x537e18 = await openai[_0x878967(0xef)]["completions"]["create"]({
-    model: _0x878967(0xe1),
+  const _0x21d3d3 = _0x137733;
+  event[_0x21d3d3(0x1e2)]();
+  const _0x2dab45 = document[_0x21d3d3(0x20f)](_0x21d3d3(0x217)),
+    _0x1f4252 = _0x2dab45[_0x21d3d3(0x1fc)];
+  if (!_0x1f4252) return;
+  const _0x4ec22d = await openai["chat"]["completions"]["create"]({
+    model: _0x21d3d3(0x1ef),
     temperature: 0.8,
     max_tokens: 0x32,
     messages: [
       {
-        role: "system",
-        content: constant[_0x878967(0xf7)] + currentFriend[_0x878967(0xf4)],
+        role: _0x21d3d3(0x1fe),
+        content: constant[_0x21d3d3(0x214)] + currentFriend[_0x21d3d3(0x1f9)],
       },
-      { role: _0x878967(0xd9), content: _0x194b55 },
+      { role: _0x21d3d3(0x210), content: _0x1f4252 },
     ],
   });
-  addNewAnswer(
-    _0x537e18[_0x878967(0xfa)][0x0][_0x878967(0xe4)][_0x878967(0xf4)]
-  );
+  addNewAnswer(_0x4ec22d["choices"][0x0][_0x21d3d3(0x1e9)]["content"]);
 }
-function addNewAnswer(_0x5e3c26) {
-  const _0x4e92e4 = _0x4be309;
-  if (!_0x5e3c26) return;
-  const _0x12111c =
-    _0x4e92e4(0xdf) +
+function addNewAnswer(_0x26d1cb) {
+  const _0x4f9946 = _0x137733;
+  if (!_0x26d1cb) return;
+  const _0x7e7368 =
+    _0x4f9946(0x204) +
     currentFriendType +
-    "\x20px-4\x20py-2\x20rounded-lg\x20mr-auto\x22>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20" +
-    _0x5e3c26 +
-    "\x0a\x20\x20\x20\x20\x20\x20\x20\x20</div>\x0a\x20\x20\x20\x20\x20\x20</li>";
-  CHAT[_0x4e92e4(0xd6)]("beforeend", _0x12111c),
-    (CHAT[_0x4e92e4(0x108)] = CHAT[_0x4e92e4(0x104)]);
+    _0x4f9946(0x208) +
+    _0x26d1cb +
+    _0x4f9946(0x1f0);
+  CHAT["insertAdjacentHTML"](_0x4f9946(0x1de), _0x7e7368),
+    (CHAT[_0x4f9946(0x215)] = CHAT["scrollHeight"]);
 }
